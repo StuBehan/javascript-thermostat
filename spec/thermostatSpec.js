c98a4a2a4985990686d13b61c1fd60d463a92952
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-04-07 16:59:54 +0100

    added default temperature and method to increase

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,4 +6,15 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
   
+  describe('the thermostat has a temperature', function() {
+    it('defaults to 20 degrees', function() {
+      expect(thermostat.getTemperature()).toEqual(20);
+    })
+
+    it('can be increased by the up function', function(){
+      thermostat.up();
+      expect(thermostat.getTemperature()).toEqual(21);
+    })
+  })
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,13 @@
 class Thermostat {
+  constructor() {
+    this.temperature = 20;
+  }
 
+  getTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    this.temperature += 1;
+  }
 }
